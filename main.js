a61f7abba62b42ea07eaab3e5c3f098aaf4c1067
0RandomGuy0 <linivan707@gmail.com>
2025-08-05 09:46:46 +0800

    Added functionality to the buttons and display

--- main.js
+++ main.js
@@ -2,6 +2,56 @@ let num1 = "";
 let num2 = "";
 let operator = "";
 
+const digitBtn = document.querySelectorAll(".digits button");
+const operatorBtn = document.querySelectorAll(".operators button");
+const clearBtn = document.getElementById("clearBtn");
+const display = document.querySelector(".display");
+
+digitBtn.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        const digit = event.target.textContent;
+
+        if (operator){
+            num2 += digit;
+            display.textContent = num2;
+        }
+        else {
+            num1 += digit;
+            display.textContent = num1;
+        }
+    });
+});
+
+operatorBtn.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        if (num1 && num2){
+            if (num2 === "0" && operator === "/") {
+                display.textContent = "DBZERR";
+                num1 = "";
+                num2 = "";
+                operator = "";
+                return;
+            }
+            const result = operate(Number(num1), Number(num2), operator);
+            display.textContent = result;
+            num1 = String(result);
+            num2 = "";
+            operator = event.target.textContent;
+        }
+        else if (num1){
+            operator = event.target.textContent;
+        }
+
+    });
+});
+
+clearBtn.addEventListener("click", () => {
+    num1 = "";
+    num2 = "";
+    operator = "";
+    display.textContent = "000";
+});
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -15,9 +65,6 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    if (num2 === 0) {
-        throw new Error("Cannot divide by zero");
-    }
     return num1 / num2;
 }
 
@@ -32,6 +79,6 @@ function operate(num1, num2, operator) {
         case '/':
             return divide(num1, num2);
         default:
-            break
+            break;
     }
 }
