8e2edabc6cefbab5da2e6d7c6facd4d06e9dc287
0RandomGuy0 <linivan707@gmail.com>
2025-08-05 08:23:16 +0800

    Added function to call all other operations

---
+++ main.js
@@ -0,0 +1,37 @@
+let num1 = "";
+let num2 = "";
+let operator = "";
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if (num2 === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            break
+    }
+}
